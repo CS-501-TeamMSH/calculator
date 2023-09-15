f34b5d5a8bfdfa0cd335031f636fd9b23ceb29e0
md499 <mdhir@bu.edu>
2023-09-15 16:50:39 -0400

    'made UI and initial code in MainActivity.kt' '

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val buttonPlus = findViewById<Button>(R.id.showInput)
+        val buttonMinus = findViewById<Button>(R.id.showInput2)
+        val buttonMultiply = findViewById<Button>(R.id.showInput3)
+        val buttonDivide = findViewById<Button>(R.id.showInput4)
+
     }
+
+
+
 }
